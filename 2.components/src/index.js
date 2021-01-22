164f573a886268c313916a512e3b3728ca35c154
grigoar <grigore.nath.j@gmail.com>
2021-01-22 09:47:50 +0200

    finished communicating with props section

--- 2.components/src/index.js
+++ 2.components/src/index.js
@@ -2,14 +2,45 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 //passing props to the child from the parent
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Grig" />
+      <ApprovalCard>
+        <div>
+          <h3>Warning!</h3>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:34PM"
+          userComment="Hello there"
+          fakerAvatar={faker.image.image()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00PM"
+          userComment="We will go on mars"
+          fakerAvatar={faker.image.image()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Grig"
+          timeAgo="Yesterday at 6:34PM"
+          userComment="Everybody dies"
+          fakerAvatar={faker.image.image()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
