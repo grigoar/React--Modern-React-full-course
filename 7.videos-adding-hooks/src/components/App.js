884a1fc5ad2e5e7a0819f2cca3fbf1c5259d07b7
grigoar <grigore.nath.j@gmail.com>
2021-01-29 12:03:56 +0200

    youtube interface videos with custom hook

--- 7.videos-adding-hooks/src/components/App.js
+++ 7.videos-adding-hooks/src/components/App.js
@@ -3,36 +3,24 @@ import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
+import useVideo from "../hooks/useVideos";
+import useVideos from "../hooks/useVideos";
 
 const App = () => {
   // state = { videos: [], selectedVideo: null };
-  const [videos, setVideos] = useState([]);
+
+  const [videos, search] = useVideos("buildings");
+
   const [selectedVideo, setSelectedVideo] = useState(null);
 
   useEffect(() => {
-    onTermSubmit("recommended");
-  }, []);
+    setSelectedVideo(videos[0]);
+  }, [videos]);
+  //
 
-  const onTermSubmit = async (term) => {
-    // console.log(term);
-    const response = await youtube.get("/search", {
-      params: {
-        q: term,
-      },
-    });
-
-    // this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] });
-    setVideos(response.data.items);
-    setSelectedVideo(response.data.items[0]);
-  };
-
-  {
-    /* <VideoList onVideoSelect={(video) => setSelectedVideo(video)} videos={videos} /> */
-    //== <VideoList onVideoSelect={setSelectedVideo} videos={videos} />
-  }
   return (
     <div className="ui container">
-      <SearchBar onFormSubmit={onTermSubmit} />
+      <SearchBar onFormSubmit={search} />
       <div className="ui grid">
         <div className="ui row">
           <div className="eleven wide column">
@@ -48,3 +36,7 @@ const App = () => {
 };
 
 export default App;
+
+//Challenge
+// import usePosts from "./usePosts";
+// const posts = usePosts();
--- 7.videos-adding-hooks/src/hooks/useVideos.js
+++ 7.videos-adding-hooks/src/hooks/useVideos.js
@@ -2,11 +2,27 @@
 import { useState, useEffect } from "react";
 import youtube from "../apis/youtube";
 
-const useVideos = () => {
+const useVideos = (defaultSearchTerm) => {
   const [videos, setVideos] = useState([]);
   useEffect(() => {
-    onTermSubmit("recommended");
-  }, []);
+    search(defaultSearchTerm);
+  }, [defaultSearchTerm]);
+
+  const search = async (term) => {
+    // console.log(term);
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+
+    // this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] });
+    setVideos(response.data.items);
+  };
+
+  //return an array or an object
+  return [videos, search];
+  //   return {videos,onTermSubmit};
 };
 
 export default useVideos;
