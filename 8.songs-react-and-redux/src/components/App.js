8348c891a0279b04a9c92907540627bb408de85e
grigoar <grigore.nath.j@gmail.com>
2021-01-29 20:30:19 +0200

    added react with redux section-updated theory

---
+++ 8.songs-react-and-redux/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import actions from "../actions/index";
+//webpack take the index.js automatically
+//default export
+import actions from "..actions";
+//named export
+import { selectSong } from "../actions";
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
---
+++ 8.songs-react-and-redux/src/reducers/index.js
@@ -0,0 +1,29 @@
+//Song list reducer
+const songsReducer = () => {
+  //static array
+  return [
+    {
+      title: "No Scrubs",
+      duration: "4:58",
+    },
+    {
+      title: "Macarena",
+      duration: "2:12",
+    },
+    {
+      title: "All Star",
+      duration: "3:33",
+    },
+    {
+      title: "I want it That Way",
+      duration: "5:03",
+    },
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+  return selectedSong;
+};
