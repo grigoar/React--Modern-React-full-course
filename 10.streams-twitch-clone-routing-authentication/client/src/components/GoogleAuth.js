56f80f44c8d19c09b0e1a3b73b9351b9cfab61a3
grigoar <grigore.nath.j@gmail.com>
2021-02-01 20:13:41 +0200

    added authentification section and some dev tools

---
+++ 10.streams-twitch-clone-routing-authentication/client/src/components/GoogleAuth.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { API_KEY } from "./api-key";
+import { connect } from "react-redux";
+import { signIn, signOut } from "../actions";
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId: API_KEY,
+          scope: "email",
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          // this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.onAuthChange(this.auth.isSignedIn.get());
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
+    });
+  }
+
+  onAuthChange = (isSignedIn) => {
+    if (isSignedIn) {
+      this.props.signIn(this.auth.currentUser.get().getId());
+    } else {
+      this.props.signOut();
+    }
+    // this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
+  renderAuthButton() {
+    if (this.props.isSignedIn === null) {
+      return null;
+    } else if (this.props.isSignedIn) {
+      return (
+        <button onClick={this.onSignOut} className="ui red google button">
+          <i className="google icon"></i>
+          Sign Out
+        </button>
+      );
+    } else
+      return (
+        <button onClick={this.onSignIn} className="ui red google button">
+          <i className="google icon"></i>
+          Sign with Google
+        </button>
+      );
+  }
+
+  render() {
+    return <div>{this.renderAuthButton()}</div>;
+  }
+}
+const mapStateToProps = (state) => {
+  return { isSignedIn: state.auth.isSignedIn };
+};
+
+export default connect(mapStateToProps, { signIn, signOut })(GoogleAuth);
