0efc60b4a4916065fb7034358c0821e7ee30e32e
grigoar <grigore.nath.j@gmail.com>
2021-02-01 12:32:24 +0200

    finished Navigation with React Router section

--- 10.streams-twitch-clone-routing-authentication/client/src/components/App.js
+++ 10.streams-twitch-clone-routing-authentication/client/src/components/App.js
@@ -1,36 +1,24 @@
 import React from "react";
 import { MemoryRouter, HashRouter, BrowserRouter, Route, Link } from "react-router-dom";
 
-const PageOne = () => {
-  return (
-    <div>
-      Page One
-      <button>
-        {/* BAD!!!!!!! */}
-        {/* <a href="/pagetwo">Click Me!</a> */}
-        <Link to="/pagetwo">Click Me!</Link>
-      </button>
-    </div>
-  );
-};
-const PageTwo = () => {
-  return (
-    <div>
-      Page Two
-      <button>
-        <Link to="/">Click Me!</Link>
-      </button>
-    </div>
-  );
-};
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
         <div>
-          <Route path="/" exact={true} component={PageOne} />
-          <Route path="/pagetwo" exact component={PageTwo} />
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
