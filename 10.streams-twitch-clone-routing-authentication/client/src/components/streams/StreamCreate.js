8f97ad1f2a218378ad64543ed218975ca0a7d3ab
grigoar <grigore.nath.j@gmail.com>
2021-02-02 10:44:41 +0200

    finished handling forms with Redux Form section

--- 10.streams-twitch-clone-routing-authentication/client/src/components/streams/StreamCreate.js
+++ 10.streams-twitch-clone-routing-authentication/client/src/components/streams/StreamCreate.js
@@ -1,7 +1,63 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
+class StreamCreate extends React.Component {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+
+  renderInput = ({ input, label, meta }) => {
+    // console.log({input});
+    // console.log(meta);
+    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
+      </div>
+    );
+    // return <input {...formProps.input} />;
+    // return <input onChange={formProps.input.onChange} value={formProps.input.value}></input>;
+  };
+
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  render() {
+    // console.log(this.props);
+    return (
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field name="description" component={this.renderInput} label="Enter Description" />
+
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.title) {
+    //if the user didn't enter a title
+    errors.title = "You must enter a title";
+  }
+  if (!formValues.description) {
+    errors.description = "You must enter a description";
+  }
+
+  return errors;
 };
 
-export default StreamCreate;
+export default reduxForm({
+  form: "streamCreate",
+  validate: validate,
+})(StreamCreate);
--- 10.streams-twitch-clone-routing-authentication/client/src/reducers/index.js
+++ 10.streams-twitch-clone-routing-authentication/client/src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from "redux";
+//we can rename the named export
+import { reducer as formReducer } from "redux-form";
 import authReducer from "./authReducer";
 
 export default combineReducers({
   auth: authReducer,
+  form: formReducer,
 });
